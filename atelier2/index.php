497acf6ff348bd36c98829e814f78989ec4edd99
Plaplaplaplapla <deprade-eliott@outlook.fr>
2025-11-05 11:02:17 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Vérification simple du username et de son password.
     // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
     if ($username === 'admin' && $password === 'secret') {
-        $eljeton = bin2hex(random_bytes(16)) 
+        $eljeton = bin2hex(random_bytes(16)); 
         setcookie('authToken', $eljeton, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
