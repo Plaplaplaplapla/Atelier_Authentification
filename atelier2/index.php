d88efc17a888ad14b6fd26e6b79867f8bb6ca8ea
Plaplaplaplapla <deprade-eliott@outlook.fr>
2025-11-05 11:27:12 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -2,6 +2,11 @@
 // Démarrer une session utilisateur qui sera en mesure de pouvoir gérer les Cookies
 session_start();
 
+if (isset($_COOKIE['authToken']) && $_COOKIE['authToken'] === 'bin2hex(random_bytes(16))') {
+    header('Location: page_admin.php');
+    exit();
+}
+
 // Gérer la soumission du formulaire
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
@@ -9,15 +14,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Vérification simple du username et de son password.
     // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
-    if ($username === 'admin' && $password === 'secret') {
-        $eljeton = bin2hex(random_bytes(16)); 
-        setcookie('authToken', $eljeton, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+       if ($username === 'admin' && $password === 'secret') {
+        setcookie('authToken', 'bin2hex(random_bytes(16))', time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } else {
         $error = "Nom d'utilisateur ou mot de passe incorrect.";
     }
-}
 ?>
 
 <!DOCTYPE html>
